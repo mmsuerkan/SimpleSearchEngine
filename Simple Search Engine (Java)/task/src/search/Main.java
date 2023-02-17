85ded8e829e51e8d05dbbff254161b85b4bb54bc
Mustafa Mert Süerkan <mmsuerkan@gmail.com>
2023-02-17 21:52:39 +0300

    String theory

--- Simple Search Engine (Java)/task/src/search/Main.java
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -1,7 +1,23 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String word = scanner.next();
+
+        if(line.contains(word)) {
+            String[] wordList = line.split(" ");
+            for (int i = 0; i < wordList.length; i++) {
+                if (wordList[i].equals(word)) {
+                    System.out.println(i + 1);
+                }
+            }
+        } else {
+            System.out.println("Not found");
+        }
+
     }
 }
